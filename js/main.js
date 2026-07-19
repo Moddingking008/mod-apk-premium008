795b723bd7600b9592a9c0b5c3ca3965183ff0cf
Samarth Gupta <sanidhya008kasaudhan@gmail.com>
2026-07-19 20:50:24 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,73 +1,59 @@
-const apps = [
-{
-name:"Netflix Premium",
-image:"https://picsum.photos/300/300?1",
-version:"v9.12.0",
-link:"#"
-},
-{
-name:"Spotify Premium",
-image:"https://picsum.photos/300/300?2",
-version:"v9.0.58",
-link:"#"
-},
-{
-name:"YouTube Premium",
-image:"https://picsum.photos/300/300?3",
-version:"v20.25",
-link:"#"
-},
-{
-name:"Alight Motion",
-image:"https://picsum.photos/300/300?4",
-version:"v5.0.0",
-link:"#"
-},
-{
-name:"CapCut Pro",
-image:"https://picsum.photos/300/300?5",
-version:"v15.8",
-link:"#"
-},
-{
-name:"Kuku TV Premium",
-image:"https://picsum.photos/300/300?6",
-version:"Latest",
-link:"#"
-}
-];
+const container = document.getElementById("apps");
 
-const container=document.getElementById("apps");
+fetch("data/apps.json")
+.then(response => response.json())
+.then(apps => {
 
-function loadApps(list){
-container.innerHTML="";
+showApps(apps);
 
-list.forEach(app=>{
-container.innerHTML+=`
-<div class="card">
-<img src="${app.image}">
-<div class="card-content">
-<h3>${app.name}</h3>
-<p>${app.version}</p>
-<a href="app.html?app=${encodeURIComponent(app.name)}" class="btn">
-View Details
-</a>
-</div>
-</div>
-`;
-});
-}
+document.getElementById("search").addEventListener("input", function () {
 
-loadApps(apps);
+const value = this.value.toLowerCase();
 
-document.getElementById("search").addEventListener("input",function(){
-
-const value=this.value.toLowerCase();
-
-const result=apps.filter(app=>
+const filtered = apps.filter(app =>
 app.name.toLowerCase().includes(value)
 );
 
-loadApps(result);
+showApps(filtered);
 
 });
+
+});
+
+function showApps(apps){
+
+container.innerHTML = "";
+
+apps.forEach(app=>{
+
+container.innerHTML += `
+
+<div class="card">
+
+<img src="${app.logo}" alt="${app.name}">
+
+<div class="card-content">
+
+<h3>${app.name}</h3>
+
+<p>Version : ${app.version}</p>
+
+<p>Size : ${app.size}</p>
+
+<p>⭐ ${app.rating}</p>
+
+<a href="app.html?app=${encodeURIComponent(app.name)}" class="btn">
+
+View Details
+
+</a>
+
+</div>
+
+</div>
+
+`;
+
+});
+
+             }
